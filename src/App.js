a4c769726c8a37fe41094fb40413cd9154e50546
Hieu Nguyen <hieu.nguyen@s3corp.com.vn>
2020-09-21 18:12:54 +0700

    onSelect profile

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import './assets/css/tooltip.css';
 import './assets/css/main.css';
 import './assets/css/profile.css';
 
-function App() {
+const App = () => {
   const [profile, setProfile] = useState([
-    {id: "profile1", name: "default", title: "Default", actived: true},
-    {id: "profile2", name: "game", title: "Game", actived: false},
-    {id: "profile3", name: "movie", title: "Movie", actived: false},
-    {id: "profile4", name: "music", title: "Music", actived: false},
-    {id: "custom1", name: "custom", title: "Custom 1", actived: false},
-    {id: "custom2", name: "custom", title: "demo long text demo long text demo", actived: false},
+    { id: "profile1", name: "default", title: "Default", actived: true },
+    { id: "profile2", name: "game", title: "Game", actived: false },
+    { id: "profile3", name: "movie", title: "Movie", actived: false },
+    { id: "profile4", name: "music", title: "Music", actived: false },
+    { id: "custom1", name: "custom", title: "Custom 1", actived: false },
+    { id: "custom2", name: "custom", title: "demo long text demo long text demo", actived: false },
   ])
   const [selected, setSelected] = useState();
+
+  const selectProfile = (i) => {
+    let newProfile = [...profile];
+    let index = newProfile.findIndex((x) => {
+      return x === newProfile.filter((x) => {
+        return x.actived === true;
+      })[0];
+    });
+    newProfile[index].actived = false;
+    newProfile[i].actived = true;
+    console.log(newProfile);
+    setProfile(newProfile);
+  }
+
   return (
     <div className="main-container">
       <div className="thx-wrapper flex">
@@ -23,21 +37,16 @@ function App() {
           </div>
           <div id="profileWrapper" className="drawer-select flex">
             <div id="profileList" className="scrollable">
-              {profile.map((item,i) => 
-                <div 
+              {profile.map((item, i) =>
+                <div
                   key={i}
-                  id={item.id} 
-                  className={"profile-item " 
-                            + item.name 
-                            + (item.name==="custom" ? "" : " no-edit")
-                            + (item.actived ? " active" : "")
-                            }
-                  onClick={() => {
-                    let newProfile = [...profile];
-                    newProfile.forEach((x) => {
-
-                    })
-                  }}>
+                  id={item.id}
+                  className={"profile-item "
+                    + item.name
+                    + (item.name === "custom" ? "" : " no-edit")
+                    + (item.actived ? " active" : "")
+                  }
+                  onClick={() => selectProfile(i)}>
                   {item.title}
                 </div>
               )}
