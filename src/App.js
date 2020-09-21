a68ef0b35120e9e370a27db1949f054041d5e606
Hieu Nguyen <hieu.nguyen@s3corp.com.vn>
2020-09-21 17:43:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './assets/css/tooltip.css';
+import './assets/css/main.css';
+import './assets/css/profile.css';
 
 function App() {
+  const [profile, setProfile] = useState([
+    {id: "profile1", name: "default", title: "Default", actived: true},
+    {id: "profile2", name: "game", title: "Game", actived: false},
+    {id: "profile3", name: "movie", title: "Movie", actived: false},
+    {id: "profile4", name: "music", title: "Music", actived: false},
+    {id: "custom1", name: "custom", title: "Custom 1", actived: false},
+    {id: "custom2", name: "custom", title: "demo long text demo long text demo", actived: false},
+  ])
+  const [selected, setSelected] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="thx-wrapper flex">
+        <div className="thx-drawer flex">
+          <div className="main-title">
+            Profile List
+          </div>
+          <div id="profileWrapper" className="drawer-select flex">
+            <div id="profileList" className="scrollable">
+              {profile.map((item,i) => 
+                <div 
+                  key={i}
+                  id={item.id} 
+                  className={"profile-item " 
+                            + item.name 
+                            + (item.name==="custom" ? "" : " no-edit")
+                            + (item.actived ? " active" : "")
+                            }
+                  onClick={() => {
+                    let newProfile = [...profile];
+                    newProfile.forEach((x) => {
+
+                    })
+                  }}>
+                  {item.title}
+                </div>
+              )}
+              <input
+                id="profileRename"
+                className="profile-item"
+                placeholder="Enter Profile Name"
+                maxlength="25"
+              />
+            </div>
+            <div className="toolbar flex">
+              <div className="icon add" id="profileAdd"></div>
+              <div className="icon edit" id="profileEdit"></div>
+              <div className="icon delete" id="profileDelete"></div>
+
+              <div className="icon down" id="profileDown"></div>
+              <div className="icon up disabled" id="profileUp"></div>
+            </div>
+            <div id="profileDelCfm" className="profile-del alert flex">
+              <div className="title">delete eq</div>
+              <div className="body-text t-center" id="delName">delete eq</div>
+              <div className="thx-btn" id="cfmDelete">delete</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="thx-window">
+          <div className="sub-title flex">
+            <h1 id="eqTitle" className="eq-title">Default</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
